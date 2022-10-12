4ee3908fb838a73a8c4b37abf4c492e3e52af65a
Richard Sim <rdsim8589@gmail.com>
2022-10-12 18:07:00 -0400

    completed page template for landing page

---
+++ scripts.js
@@ -0,0 +1,2 @@
+// do javascript to get all the information input on the form once Create Account is clicked
+// change the color of the "password" and "password confirm" to red if they didn't match
